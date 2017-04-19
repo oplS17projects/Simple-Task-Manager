3d19997e9b541f9d441923dcc1ec09106339094d
Connor Cooper <cotiger3@gmail.com>
2017-04-19 13:36:49 -0400

    re-added overwritten work

--- task-client.rkt
+++ task-client.rkt
@@ -6,6 +6,20 @@
 ; file path to store json data in
 (define taskFile "tasks")
 
+; list of all fields in a task object
+(define taskFields (list
+                    'name
+                    'due
+                    'ID
+                    'priority))
+
+(define emptyTask
+  (make-immutable-hasheq
+   (map (lambda (key)
+          (cons key ""))
+        taskFields)))
+
+
 ; read the task list from the json file
 (define (readTaskList)
   (call-with-input-file taskFile read-json))
@@ -14,11 +28,48 @@
 (define (displayTasks)
   (define (iter list)
     (if (null? list)
-        #t
+        (void)
         (begin
           (writeTaskObject (car list))
           (iter (cdr list)))))
   (iter (hash-ref (readTaskList) 'tasks)))
+
+; Find a task by ID and change the given fields to their values.
+; the fields argument is a list of (key, value) pairs to be updated to the given values
+(define (editTask id fields)
+  (let ([oldTask (getTaskWithID id)])
+    (begin
+      (deleteTask id) ;delete old version of the task
+      (let ([taskList (readTaskList)]
+            [out (open-output-file taskFile #:exists 'truncate)])
+        (begin
+          (write-json ;add new version of task to task list
+           (makeTaskList
+            (cons (changeTaskObject oldTask fields) (hash-ref taskList 'tasks))
+            (hash-ref taskList 'nextID))
+           out)
+          (close-output-port out))))))
+
+
+; delete a task with a given ID
+(define (deleteTask id)
+  (let ([taskList (readTaskList)]
+        [out (open-output-file taskFile #:exists 'truncate)])
+    (begin
+      (write-json
+       (makeTaskList
+        (filter (lambda (t) (not (taskID? t id))) (hash-ref taskList 'tasks))
+        (hash-ref taskList 'nextID))
+       out)
+      (close-output-port out))))
+
+(define (getTaskWithID id)
+  (let ([taskList (readTaskList)])
+    (car (filter (lambda (t) (taskID? t id)) (hash-ref taskList 'tasks)))))
+
+; returns true if a task has the same ID as the given ID
+(define (taskID? task id)
+  (= id (hash-ref task 'ID)))
 
 ; Display a single Task object
 (define (writeTaskObject Task)
@@ -26,35 +77,51 @@
     (write "Task: ")
     (write (hash-ref Task 'name))
     (write ", Due: ")
-    (writeln (hash-ref Task 'due))))
+    (write (hash-ref Task 'due))
+    (write ", Priority: ")
+    (writeln (hash-ref Task 'priority))))
+
+; change the given fields in a task object to the given values
+(define (changeTaskObject task fields)
+  (make-immutable-hasheq
+   (map (lambda (key)
+          (let ([field (filter (lambda (k) (equal? (car k) key)) fields)])
+            (if (null? field) ; if the field is being changed, take the new one. else use the old one.
+                (cons key (hash-ref task key))
+                (car field))))
+        taskFields)))
 
 ; Create a new task object
-(define (makeTask name due)
-  (make-immutable-hasheq
-   (list
-    (cons 'name name)
-    (cons 'due due))))
+(define (makeTask fields)
+  (changeTaskObject emptyTask fields))
 
 ; Create a new task list object
-(define (makeTaskList taskPairs)
+(define (makeTaskList taskPairs id)
   (make-immutable-hasheq
    (list
-    (cons
-     'tasks
-     taskPairs))))
+    (cons 'nextID id)
+    (cons 'tasks taskPairs))))
 
 ; Create a new task with specified name and date and add it to the task list. Write result in json file.
-(define (addTask name due)
+(define (addTask id fields)
   (let ([taskList (readTaskList)]
         [out (open-output-file taskFile #:exists 'truncate)])
     (begin
       (write-json
        (makeTaskList
         (cons ; add new task to list of existing tasks
-         (makeTask name due)
-         (hash-ref taskList 'tasks)))
+         (makeTask
+          (cons
+           (cons 'ID (if (equal? id 'auto) ; if an ID is provided, use it. Otherwise generate automatically
+                         (hash-ref taskList 'nextID)
+                         id))
+           fields))
+         (hash-ref taskList 'tasks))
+        (if (equal? id 'auto) ; if we auto-generated the task ID, increment the 'next ID' value on the list object
+            (+ (hash-ref taskList 'nextID) 1)
+            (hash-ref taskList 'nextID)))
        out)
-      (close-output-port out))))     
+      (close-output-port out))))
 
 ; Create a new task list to override the existing one in json file
 (define (overrideTaskList taskPairs)
